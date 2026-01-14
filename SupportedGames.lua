9001352b87309b93a311cb1dfb57dd58066e8858
PickleTalk2 <johnvincent.orcullo22@gmail.com>
2026-01-14 14:42:10 +0800

    Update SupportedGames.lua

--- SupportedGames.lua
+++ SupportedGames.lua
@@ -1,4 +1,4 @@
-return Games {
+return {
     -- Escape tsunami for brainrot
-    131623223084840 = "https://raw.githubusercontent.com/PickleTalk2/luaProjects/refs/heads/main/Games/Escape-Tsunami-For-Brainrots.lua"
+    [131623223084840] = "https://raw.githubusercontent.com/PickleTalk2/luaProjects/refs/heads/main/Games/Escape-Tsunami-For-Brainrots.lua"
 }
