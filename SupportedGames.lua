fb59c4eed09a641ae37cc450343a288efc0cef45
PickleTalk2 <johnvincent.orcullo22@gmail.com>
2026-01-14 14:13:14 +0800

    Create SupportedGames.lua

---
+++ SupportedGames.lua
@@ -0,0 +1,4 @@
+return {
+    -- Escape tsunami for brainrot
+    131623223084840 = "https://raw.githubusercontent.com/PickleTalk2/luaProjects/refs/heads/main/Games/Escape-Tsunami-For-Brainrots.lua"
+}
