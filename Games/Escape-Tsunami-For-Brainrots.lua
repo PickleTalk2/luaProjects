7f59f23745ed87b4c76854b3acefdc780fcdfd1f
PickleTalk2 <johnvincent.orcullo22@gmail.com>
2026-01-21 12:42:26 +0800

    Update Escape-Tsunami-For-Brainrots.lua

--- Games/Escape-Tsunami-For-Brainrots.lua
+++ Games/Escape-Tsunami-For-Brainrots.lua
@@ -971,7 +971,7 @@ local function toggleSlapAura(state)
                         if targetHrp then
                             local hrp = character:FindFirstChild("HumanoidRootPart")
                             if hrp then
-                                task.wait(0.5)a
+                                task.wait(0.5)
                                 hrp.CFrame = targetHrp.CFrame * CFrame.new(0, 0, 3)
                                 
                                 if States.DebugMode then
