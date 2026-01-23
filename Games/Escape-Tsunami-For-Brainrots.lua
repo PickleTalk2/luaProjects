cb98ad7c29c0b84854251e9d70e767a9416778c2
PickleTalk2 <johnvincent.orcullo22@gmail.com>
2026-01-23 20:45:50 +0800

    Update Escape-Tsunami-For-Brainrots.lua

--- Games/Escape-Tsunami-For-Brainrots.lua
+++ Games/Escape-Tsunami-For-Brainrots.lua
@@ -2346,7 +2346,7 @@ local function toggleAutoTeleport(state)
             })
             
             local targetX = gap9Target.Position.X
-            local targetY = -6
+            local targetY = -8
             local targetZ = -1
             
             while States.AutoTeleport and States.AutoTeleportActive do
