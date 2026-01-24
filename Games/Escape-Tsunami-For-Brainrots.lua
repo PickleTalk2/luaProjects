ba6e7f77f1b8c783be1998404f5c1f7c71617cb5
PickleTalk2 <johnvincent.orcullo22@gmail.com>
2026-01-24 23:57:31 +0800

    Update Escape-Tsunami-For-Brainrots.lua

--- Games/Escape-Tsunami-For-Brainrots.lua
+++ Games/Escape-Tsunami-For-Brainrots.lua
@@ -1471,18 +1471,17 @@ function executeCelestialSteal()
         end
 
         task.wait(0.05)
+        hrp.Anchored = false
         hrp.CFrame = celestialRoot.CFrame
-        task.wait(0.05)
+        task.wait(0.1)
         local takePrompt = celestialRoot:FindFirstChild("TakePrompt")
         if takePrompt and takePrompt:IsA("ProximityPrompt") then
             fireproximityprompt(takePrompt)
         end
-        task.wait(0.05)
-        if takePrompt and takePrompt:IsA("ProximityPrompt") then
-            fireproximityprompt(takePrompt)
-        end
+        task.wait(0.1)
         hrp.CFrame = CFrame.new(celestialX, 65, celestialZ)
-        
+        task.wait(0.05)
+        hrp.Anchored = true
         local distanceToReturn = math.abs(celestialX - 120)
         local returnTweenSpeed = 220
         local returnTweenTime = distanceToReturn / returnTweenSpeed
