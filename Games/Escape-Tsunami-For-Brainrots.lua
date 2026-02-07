9f37c443d929da681f2423d83abf70684df4aa08
PickleTalk2 <johnvincent.orcullo22@gmail.com>
2026-02-07 14:40:14 +0800

    Update Escape-Tsunami-For-Brainrots.lua

--- Games/Escape-Tsunami-For-Brainrots.lua
+++ Games/Escape-Tsunami-For-Brainrots.lua
@@ -1,4 +1,4 @@
-local success, WindUI = pcall(function()
+1local success, WindUI = pcall(function()
     return loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 end)
 
